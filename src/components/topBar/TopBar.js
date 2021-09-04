41b312cb4c208e332f2cb4d69d4db2f8feaf428b
Jhonnatan Gomes <jhonnatangomes05@gmail.com>
2021-09-04 12:44:59 -0300

    go back button implemented on top of page

--- src/components/topBar/TopBar.js
+++ src/components/topBar/TopBar.js
@@ -1,9 +1,15 @@
 import "./topBar.css";
+import { useLocation, useHistory } from "react-router";
 
 export default function TopBar (){
+
+    const location = useLocation();
+    const history = useHistory();
+
     return (
         <div className="top-bar">
-            CINEFLEX
+            {location.pathname === "/cineflex" || location.pathname === "/" ? "" : <ion-icon name="caret-back-circle-outline" onClick={history.goBack}></ion-icon>}
+            <div className="app-title">CINEFLEX</div>
         </div>
     );
 }
